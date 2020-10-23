3c3deac7161bf1c9f75016abf68f05b3a81389d3
Haley Myers <haleynmyers@gmail.com>
2020-10-23 11:20:50 -0500

    making login authentification files and routes

--- models/user.js
+++ models/user.js
@@ -1,12 +1,17 @@
+// importing modules 
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose'); 
 
 const userSchema = new Schema({
   username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true }
+  email: { type: String, required: true }
 });
 
+// plugin for passport-local-mongoose 
+UserSchema.plugin(passportLocalMongoose); 
+
+// export userschema 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
