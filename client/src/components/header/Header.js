aa0c4626eaf6b9be12cc3150c2aa26e35b004fb7
Payton Banks <paytonbanks@Mac-Payton.local>
2020-10-28 12:09:35 -0400

    replaced Header component, add CSS with desktop and mobile view ports(640 breakpoint, only has the desktop photo. moved README assets into public/images

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,50 +1,19 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import './style.css';
+import desktopImage from './header.jpg';
+import mobileImage from './header.jpg';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 600,
-  },
-});
+const Background = () => {
+    const imageUrl = window.innerWidth >= 1200 ? desktopImage : mobileImage;
 
-export default function Header() {
+    return (
+        <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
+            <div className="App-content">
+                <h1>Bill Tracker</h1>
+                <p>Keep track of monthly bills</p>
+            </div>
+        </div>
+    );
+};
 
-  const classes = useStyles();
-
-  return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Money Header"
-          height="300"
-          image="./header.jpg"
-          title="Bill Tracker"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Bill Tracker
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Pay Monthly Bill Easily 
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Open Link 1
-        </Button>
-        <Button size="small" color="primary">
-          Open Link 2
-        </Button>
-      </CardActions>
-    </Card>
-  );
-}
+export default Background;
