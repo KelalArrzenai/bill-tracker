a5bc7f99ef971a3cff75119a82bb7d430b941e2a
Payton Banks <paytonbanks@Mac-Payton.local>
2020-10-29 13:09:09 -0400

    header added top with repsonsive

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,17 +3,19 @@ import './style.css';
 import desktopImage from './desktopImage.jpg';
 import mobileImage from './mobileImage.jpg';
 
-const Background = () => {
-    const imageUrl = window.innerWidth >= 1200 ? desktopImage : mobileImage;
-
+const Header = () => {
+  
+    const imageUrl = window.innerWidth = 1200 ? desktopImage : mobileImage;
     return (
+      
         <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
             <div className="App-content">
                 <h1>Bill Tracker</h1>
                 <p>Keep track of monthly bills</p>
             </div>
         </div>
+
     );
 };
 
-export default Background;
+export default Header;
